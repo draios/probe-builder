b9cda3a09b4a770439a8e59117dc253ac3a8ba1a
Daniele De Lorenzi <daniele.delorenzi@sysdig.com>
2022-10-26 15:53:16 +0200

    Introduced support to AlmaLinux and RockyLinux on kernel-crawler

--- probe_builder/kernel_crawler/__init__.py
+++ probe_builder/kernel_crawler/__init__.py
@@ -1,8 +1,10 @@
+from .almalinux import AlmaLinuxMirror
 from .amazonlinux import AmazonLinux1Mirror, AmazonLinux2Mirror, AmazonLinux2022Mirror
 from .centos import CentosMirror
 from .fedora import FedoraMirror
 from .oracle import Oracle6Mirror, Oracle7Mirror, Oracle8Mirror
 from .photon_os import PhotonOsMirror
+from .rockylinux import RockyLinuxMirror
 
 from .debian import DebianMirror
 from .ubuntu import UbuntuMirror
@@ -10,6 +12,7 @@ from .ubuntu import UbuntuMirror
 from .flatcar import FlatcarMirror
 
 DISTROS = {
+    'AlmaLinux': AlmaLinuxMirror,
     'AmazonLinux': AmazonLinux1Mirror,
     'AmazonLinux2': AmazonLinux2Mirror,
     'AmazonLinux2022': AmazonLinux2022Mirror,
@@ -19,6 +22,7 @@ DISTROS = {
     'Oracle7': Oracle7Mirror,
     'Oracle8': Oracle8Mirror,
     'PhotonOS': PhotonOsMirror,
+    'RockyLinux': RockyLinuxMirror,
 
     'Debian': DebianMirror,
     'Ubuntu': UbuntuMirror,
---
+++ probe_builder/kernel_crawler/almalinux.py
@@ -0,0 +1,20 @@
+from . import repo
+from . import rpm
+
+def v8_only(ver):
+    return ver.startswith('8')
+
+def v9_only(ver):
+    return ver.startswith('9')
+
+class AlmaLinuxMirror(repo.Distro):
+    def __init__(self, arch):
+        mirrors = [
+            # AlmaLinux 8
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'BaseOS/' + arch + '/os/', v8_only),
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'AppStream/' + arch + '/os/', v8_only),
+            # AlmaLinux 9
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'BaseOS/' + arch + '/os/', v9_only),
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'AppStream/' + arch + '/os/', v9_only),
+        ]
+        super(AlmaLinuxMirror, self).__init__(mirrors, arch)
