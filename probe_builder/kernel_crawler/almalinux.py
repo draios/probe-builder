f67c51ba8b89e6f583a4103f7b7a23de3c901fa5
Daniele De Lorenzi <daniele.delorenzi@sysdig.com>
2022-10-27 10:43:51 +0200

    Fixed issue with arch parameter

--- probe_builder/kernel_crawler/almalinux.py
+++ probe_builder/kernel_crawler/almalinux.py
@@ -8,13 +8,13 @@ def v9_only(ver):
     return ver.startswith('9')
 
 class AlmaLinuxMirror(repo.Distro):
-    def __init__(self, arch):
+    def __init__(self):
         mirrors = [
             # AlmaLinux 8
-            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'BaseOS/' + arch + '/os/', v8_only),
-            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'AppStream/' + arch + '/os/', v8_only),
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'BaseOS/x86_64/os/', v8_only),
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'AppStream/x86_64/os/', v8_only),
             # AlmaLinux 9
-            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'BaseOS/' + arch + '/os/', v9_only),
-            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'AppStream/' + arch + '/os/', v9_only),
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'BaseOS/x86_64/os/', v9_only),
+            rpm.RpmMirror('http://repo.almalinux.org/almalinux/', 'AppStream/x86_64/os/', v9_only),
         ]
-        super(AlmaLinuxMirror, self).__init__(mirrors, arch)
+        super(AlmaLinuxMirror, self).__init__(mirrors)
--- probe_builder/kernel_crawler/rockylinux.py
+++ probe_builder/kernel_crawler/rockylinux.py
@@ -8,16 +8,16 @@ def v9_only(ver):
     return ver.startswith('9')
 
 class RockyLinuxMirror(repo.Distro):
-    def __init__(self, arch):
+    def __init__(self):
         mirrors = [
             # Rocky Linux 8
-            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'BaseOS/' + arch + '/os/', v8_only),
-            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'AppStream/' + arch + '/os/', v8_only),
-            rpm.RpmMirror('http://dl.rockylinux.org/vault/rocky/', 'BaseOS/' + arch + '/os/', v8_only),
+            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'BaseOS/x86_64/os/', v8_only),
+            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'AppStream/x86_64/os/', v8_only),
+            rpm.RpmMirror('http://dl.rockylinux.org/vault/rocky/', 'BaseOS/x86_64/os/', v8_only),
             # Rocky Linux 9
-            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'BaseOS/' + arch + '/os/', v9_only),
-            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'AppStream/' + arch + '/os/', v9_only),
+            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'BaseOS/x86_64/os/', v9_only),
+            rpm.RpmMirror('http://dl.rockylinux.org/pub/rocky/', 'AppStream/x86_64/os/', v9_only),
             # Valut repo not yet available for Rocky Linux 9
-            #rpm.RpmMirror('http://dl.rockylinux.org/vault/rocky/', v9_only, 'BaseOS/' + arch + '/os/'),
+            #rpm.RpmMirror('http://dl.rockylinux.org/vault/rocky/', 'BaseOS/x86_64/os/', v9_only),
         ]
-        super(RockyLinuxMirror, self).__init__(mirrors, arch)
+        super(RockyLinuxMirror, self).__init__(mirrors)
