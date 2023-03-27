draios/probe-builder

commit 151f47a13e435f8e7085084b6c3bb967ab3382c6

add support for distro release filtering (#68)
* add support for distro release filtering

add an extra parameter to add the possibility to filter
packages for a given distro release.
The main use case is ubuntu, whose list of packages
(considering all releases) is simply humongous.

This way we can filter by adding -R "impish".

* distro filtering: rework so to have a global filtering object

* make CrawlerFilter a namedtuple

* fix rpm filtering

probe_builder/__init__.py
@@ -61,8 +61,8 @@ class CrawlDistro(object):
         self.distro_builder = self.distro_obj.builder()
         self.crawler_distro = crawler_distro
 
-    def get_kernels(self, workspace, _packages, download_config, filter):
-        return self.distro_builder.crawl(workspace, self.distro_obj, self.crawler_distro, download_config, filter)
+    def get_kernels(self, workspace, _packages, download_config, crawler_filter):
+        return self.distro_builder.crawl(workspace, self.distro_obj, self.crawler_distro, download_config, crawler_filter)
 
 class LocalDistro(object):
 
@@ -70,7 +70,7 @@ class LocalDistro(object):
         self.distro_obj = Distro(distro, builder_distro)
         self.distro_builder = self.distro_obj.builder()
 
-    def get_kernels(self, _workspace, packages, _download_config, filter):
+    def get_kernels(self, _workspace, packages, _download_config, _crawler_filter):
         return self.distro_builder.batch_packages(packages)
 
 
@@ -109,7 +109,8 @@ def cli(debug):
 @click.option('-d', '--download-concurrency', type=click.INT, default=1)
 @click.option('-j', '--jobs', type=click.INT, default=len(os.sched_getaffinity(0)))
 @click.option('-k', '--kernel-type', type=click.Choice(sorted(CLI_DISTROS.keys())))
-@click.option('-f', '--filter', default='')
+@click.option('-R', '--distro-filter', default='')
+@click.option('-f', '--kernel-filter', default='')
 @click.option('-p', '--probe-name')
 @click.option('-r', '--retries', type=click.INT, default=1)
 @click.option('-s', '--source-dir')
@@ -117,7 +118,8 @@ def cli(debug):
 @click.option('-v', '--probe-version')
 @click.argument('package', nargs=-1)
 def build(builder_image_prefix,
-          download_concurrency, jobs, kernel_type, filter, probe_name, retries,
+          download_concurrency, jobs, kernel_type, distro_filter,
+          kernel_filter, probe_name, retries,
           source_dir, download_timeout, probe_version, package):
     workspace_dir = os.getcwd()
     builder_source = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,7 +132,9 @@ def build(builder_image_prefix,
     distro = distro_obj.distro_obj
     download_config = DownloadConfig(download_concurrency, download_timeout, retries, None)
 
-    kernels = distro_obj.get_kernels(workspace, package, download_config, filter)
+    crawler_filter = kernel_crawler.repo.CrawlerFilter(distro_filter=distro_filter, kernel_filter=kernel_filter)
+
+    kernels = distro_obj.get_kernels(workspace, package, download_config, crawler_filter)
     kernel_dirs = distro_builder.unpack_kernels(workspace, distro.distro, kernels)
 
     with ThreadPoolExecutor(max_workers=jobs) as executor:
@@ -190,9 +194,11 @@ def build(builder_image_prefix,
 
 @click.command()
 @click.argument('distro', type=click.Choice(sorted(DISTROS.keys())))
-@click.argument('version', required=False, default='')
-def crawl(distro, version=''):
-    kernels = crawl_kernels(distro, version)
+@click.argument('distro_filter', required=False, default='')
+@click.argument('kernel_filter', required=False, default='')
+def crawl(distro, distro_filter='', kernel_filter=''):
+    crawler_filter = kernel_crawler.repo.CrawlerFilter(distro_filter=distro_filter, kernel_filter=kernel_filter)
+    kernels = crawl_kernels(distro, distro_filter, crawler_filter=crawler_filter)
     for release, packages in kernels.items():
         print('=== {} ==='.format(release))
         for pkg in packages:

probe_builder/kernel_crawler/__init__.py
@@ -32,8 +32,6 @@ DISTROS = {
     'Flatcar': FlatcarMirror,
 }
 
-
-def crawl_kernels(distro, version=''):
+def crawl_kernels(distro, crawler_filter):
     dist = DISTROS[distro]
-
-    return dist().get_package_tree(version)
+    return dist().get_package_tree(crawler_filter)

probe_builder/builder/distro/base_builder.py
@@ -9,6 +9,7 @@ import click
 from probe_builder import docker
 from probe_builder.builder import builder_image, choose_builder
 from probe_builder.kernel_crawler import crawl_kernels
+from probe_builder.kernel_crawler.repo import EMPTY_FILTER
 from probe_builder.kernel_crawler.download import download_batch
 from probe_builder.py23 import make_bytes, make_string
 
@@ -147,8 +148,8 @@ class DistroBuilder(object):
     def batch_packages(self, kernel_files):
         raise NotImplementedError
 
-    def crawl(self, workspace, distro, crawler_distro, download_config=None, filter=''):
-        kernels = crawl_kernels(crawler_distro, filter)
+    def crawl(self, workspace, distro, crawler_distro, download_config=None, crawler_filter=EMPTY_FILTER):
+        kernels = crawl_kernels(crawler_distro, crawler_filter)
         try:
             os.makedirs(workspace.subdir(distro.distro))
         except OSError as exc:

probe_builder/builder/distro/debian.py
@@ -8,6 +8,7 @@ import click
 
 from probe_builder.builder import toolkit
 from probe_builder.builder.distro.base_builder import DistroBuilder
+from probe_builder.kernel_crawler.repo import EMPTY_FILTER
 
 logger = logging.getLogger(__name__)
 pp = pprint.PrettyPrinter(depth=4)
@@ -17,12 +18,14 @@ class DebianBuilder(DistroBuilder):
     KBUILD_PACKAGE_RE = re.compile(r'linux-kbuild-(?P<major>[0-9]\.[0-9]+)_')
 
 
-    def crawl(self, workspace, distro, crawler_distro, download_config=None, filter=''):
+    def crawl(self, workspace, distro, crawler_distro, download_config=None, crawler_filter=EMPTY_FILTER):
         # for debian, we essentially want to discard the classification work performed by the crawler,
         # and batch packages together
 
         # call the parent's method
-        crawled_dict = super().crawl(workspace=workspace, distro=distro, crawler_distro=crawler_distro, download_config=download_config, filter=filter)
+        crawled_dict = super().crawl(workspace=workspace, distro=distro,
+            crawler_distro=crawler_distro, download_config=download_config,
+            crawler_filter=crawler_filter)
 
         # flatten that dictionary into a single list, retaining ONLY package urls and discarding the release altogether
         flattened_packages = [pkg for pkgs in crawled_dict.values() for pkg in pkgs]

probe_builder/builder/distro/flatcar.py
@@ -10,6 +10,7 @@ from .base_builder import DistroBuilder, to_s
 from .. import toolkit, builder_image
 from ... import crawl_kernels, docker
 from ...kernel_crawler.download import download_file
+from ...kernel_crawler.repo import EMPTY_FILTER
 
 logger = logging.getLogger(__name__)
 
@@ -69,8 +70,8 @@ class FlatcarBuilder(DistroBuilder):
         else:
             logger.info("Build for {} probe {}-{} ({}) successful".format(label, coreos_kernel_release, config_hash, release))
 
-    def crawl(self, workspace, distro, crawler_distro, download_config=None, filter=''):
-        kernels = crawl_kernels(crawler_distro, filter)
+    def crawl(self, workspace, distro, crawler_distro, download_config=None, crawler_filter=EMPTY_FILTER):
+        kernels = crawl_kernels(crawler_distro, crawler_filter=crawler_filter)
         try:
             os.makedirs(workspace.subdir(distro.distro))
         except OSError as exc:

probe_builder/builder/distro/photonos.py
@@ -1,4 +1,5 @@
 from probe_builder.builder import toolkit
+from probe_builder.kernel_crawler.repo import EMPTY_FILTER
 from .centos import CentosBuilder
 
 
@@ -10,14 +11,14 @@ class PhotonosBuilder(CentosBuilder):
             return release[: -len(".x86_64")]
         return release
 
-    def crawl(self, workspace, distro, crawler_distro, download_config=None, filter=""):
+    def crawl(self, workspace, distro, crawler_distro, download_config=None, crawler_filter=EMPTY_FILTER):
         # call the parent's method
         orig = super().crawl(
             workspace=workspace,
             distro=distro,
             crawler_distro=crawler_distro,
             download_config=download_config,
-            filter=filter,
+            crawler_filter=crawler_filter,
         )
         # make up a new list with stripped release version
         renamed = {}

probe_builder/builder/distro/ubuntu.py
@@ -7,6 +7,7 @@ import click
 
 from probe_builder.builder import toolkit
 from .base_builder import DistroBuilder
+from probe_builder.kernel_crawler.repo import EMPTY_FILTER
 
 logger = logging.getLogger(__name__)
 pp = pprint.PrettyPrinter(depth=4)
@@ -15,8 +16,8 @@ class UbuntuBuilder(DistroBuilder):
     KERNEL_VERSION_RE = re.compile(r'(?P<version>[0-9]\.[0-9]+\.[0-9]+-[0-9]+)\.(?P<update>[0-9][^_]*)')
     KERNEL_RELEASE_RE = re.compile(r'(?P<release>[0-9]\.[0-9]+\.[0-9]+-[0-9]+-[a-z0-9-]+)')
 
-    def crawl(self, workspace, distro, crawler_distro, download_config=None, filter=''):
-        crawled_dict = super().crawl(workspace=workspace, distro=distro, crawler_distro=crawler_distro, download_config=download_config, filter=filter)
+    def crawl(self, workspace, distro, crawler_distro, download_config=None, crawler_filter=EMPTY_FILTER):
+        crawled_dict = super().crawl(workspace=workspace, distro=distro, crawler_distro=crawler_distro, download_config=download_config, crawler_filter=crawler_filter)
         kernels = []
         # batch packages according to package version, e.g. '5.15.0-1001/1' as returned by the crawler
         # (which is the package version of the main 'linux-headers-5.15.0-1001-gke_5.15.0-1001.1_amd64.deb')

probe_builder/kernel_crawler/amazonlinux.py
@@ -33,7 +33,7 @@ class AmazonLinux1Mirror(repo.Distro):
     def __init__(self):
         super(AmazonLinux1Mirror, self).__init__([])
 
-    def list_repos(self):
+    def list_repos(self, crawler_filter):
         repo_urls = set()
         with click.progressbar(
                 self.AL1_REPOS, label='Checking repositories', file=sys.stderr, item_show_func=repo.to_s) as repos:
@@ -53,7 +53,7 @@ class AmazonLinux2Mirror(repo.Distro):
     def __init__(self):
         super(AmazonLinux2Mirror, self).__init__([])
 
-    def list_repos(self):
+    def list_repos(self, crawler_filter):
         repo_urls = set()
         with click.progressbar(
                 self.AL2_REPOS, label='Checking repositories', file=sys.stderr, item_show_func=repo.to_s) as repos:
@@ -88,7 +88,7 @@ class AmazonLinux2022Mirror(repo.Distro):
     def __init__(self):
         super(AmazonLinux2022Mirror, self).__init__([])
 
-    def list_repos(self):
+    def list_repos(self, crawler_filter):
         repos = []
         for base_url in self.AL202X_BASE_URLS:
             repos.extend(self.list_repos_for_url(base_url=base_url))

commit cfaa1175667c1a8d1ae70234e92d3af4f2606a7e

fix crawling bug introduced by distro filtering
With #68 we added support for distro filtering but a small
bug slipped through the crack causing the following error
when using the crawling operation:

  File "/builder/probe_builder/__init__.py", line 203, in crawl
    kernels = crawl_kernels(distro, distro_filter, crawler_filter=crawler_filter)
TypeError: crawl_kernels() got multiple values for argument 'crawler_filter'

Fix it.

probe_builder/__init__.py
@@ -200,7 +200,7 @@ def build(builder_image_prefix,
 @click.argument('kernel_filter', required=False, default='')
 def crawl(distro, distro_filter='', kernel_filter=''):
     crawler_filter = kernel_crawler.repo.CrawlerFilter(distro_filter=distro_filter, kernel_filter=kernel_filter)
-    kernels = crawl_kernels(distro, distro_filter, crawler_filter=crawler_filter)
+    kernels = crawl_kernels(distro, crawler_filter=crawler_filter)
     for release, packages in kernels.items():
         print('=== {} ==='.format(release))
         for pkg in packages: