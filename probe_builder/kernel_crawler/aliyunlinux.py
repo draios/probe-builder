6a7f739baf3cf37aedf16c6731cd0261ab8c96b7
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2023-03-28 12:53:07 +0200

    Removed duplicated mirrors
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>

--- probe_builder/kernel_crawler/aliyunlinux.py
+++ probe_builder/kernel_crawler/aliyunlinux.py
@@ -1,28 +1,19 @@
 from . import repo
 from . import rpm
 
-def v2_only(ver):
-    return ver.startswith('2')
-
-def v3_only(ver):
-    return ver.startswith('3')
+def v2_or_v3(ver):
+    return ver.startswith('2') or ver.startswith('3')
 
 class AliyunLinuxMirror(repo.Distro):
     def get_mirrors(self, crawler_filter):
         mirrors = [
-            # AliyunLinux 2
+            # AliyunLinux 2 and 3
             # Mirror list on cloud-init config example:
             # https://www.alibabacloud.com/help/en/elastic-compute-service/latest/use-alibaba-cloud-linux-2-images-in-an-on-premises-environment
-            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'os/{}/'.format(crawler_filter.machine), v2_only),
-            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'updates/{}/'.format(crawler_filter.machine), v2_only),
-            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/{}/'.format(crawler_filter.machine), v2_only),
-
-            # AliyunLinux 3
-            # Mirror list on cloud-init config example:
             # https://www.alibabacloud.com/help/en/elastic-compute-service/latest/use-alibaba-cloud-linux-3-images-in-an-on-premises-environment
-            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'os/{}/'.format(crawler_filter.machine), v3_only),
-            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'updates/{}/'.format(crawler_filter.machine), v3_only),
-            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/{}/'.format(crawler_filter.machine), v3_only),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'os/{}/'.format(crawler_filter.machine), v2_or_v3),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'updates/{}/'.format(crawler_filter.machine), v2_or_v3),
+            rpm.RpmMirror('http://mirrors.aliyun.com/alinux/', 'plus/{}/'.format(crawler_filter.machine), v2_or_v3),
 
         ]
         return mirrors
