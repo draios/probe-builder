74c67171f33639ce260edef1925bf42561ff0b1c
Paul Crooks <paul.crooks@sysdig.com>
2025-07-30 13:56:20 +0000

    chore(centos): Deprecate Centos6
    Ref: SMAGENT-9533

--- probe_builder/kernel_crawler/centos.py
+++ probe_builder/kernel_crawler/centos.py
@@ -13,9 +13,6 @@ def v8_stream(ver):
 def v9_only(ver):
     return ver.startswith('9')
 
-def v6_or_v7(ver):
-    return ver.startswith('6') or ver.startswith('7')
-
 class CentosMirror(repo.Distro):
     def get_mirrors(self, crawler_filter):
         mirrors = [
@@ -23,8 +20,8 @@ class CentosMirror(repo.Distro):
             #rpm.RpmMirror('http://mirror.centos.org/centos/', 'updates/{}/'.format(crawler_filter.machine), v7_only),
             # CentOS 8 reached end-of-life at the end of 2021, so no point looking for it
             # rpm.RpmMirror('http://mirror.centos.org/centos/', 'BaseOS/x86_64/os/', v8_only),
-            rpm.RpmMirror('http://archive.kernel.org/centos-vault/', 'os/{}/'.format(crawler_filter.machine), v6_or_v7),
-            rpm.RpmMirror('http://archive.kernel.org/centos-vault/', 'updates/{}/'.format(crawler_filter.machine), v6_or_v7),
+            rpm.RpmMirror('http://archive.kernel.org/centos-vault/', 'os/{}/'.format(crawler_filter.machine), v7_only),
+            rpm.RpmMirror('http://archive.kernel.org/centos-vault/', 'updates/{}/'.format(crawler_filter.machine), v7_only),
             rpm.RpmMirror('http://archive.kernel.org/centos-vault/', 'BaseOS/{}/os/'.format(crawler_filter.machine), v8_only),
         ]
         return mirrors
