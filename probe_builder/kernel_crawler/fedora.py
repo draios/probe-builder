9f61ae22659ef6545573a8ef5d28f18f984db444
Marco Vito Moscaritolo <mavimo@gmail.com>
2026-09-06 20:50:13 +0200

    Update Fedora mirror URLs to use osuosl.org
    The previous mirror move to IPv6 only making not possible to build drivers

--- probe_builder/kernel_crawler/fedora.py
+++ probe_builder/kernel_crawler/fedora.py
@@ -14,9 +14,9 @@ class FedoraMirror(repo.Distro):
         mirrors = [
             # Obtained by picking one from https://mirrors.fedoraproject.org/metalink?repo=fedora-37&arch=x86_64
             ### -> http://mirrors.rit.edu/fedora/fedora/linux/releases/37/Everything/x86_64/os/repodata/repomd.xml
-            rpm.RpmMirror('http://mirrors.rit.edu/fedora/fedora/linux/releases/', 'Everything/{}/os/'.format(crawler_filter.machine), repo_filter),
+            rpm.RpmMirror('https://fedora.osuosl.org/linux/releases/', 'Everything/{}/os/'.format(crawler_filter.machine), repo_filter),
             # https://mirrors.fedoraproject.org/metalink?repo=updates-released-f37&arch=x86_64
             ### -> http://mirrors.rit.edu/fedora/fedora/linux/updates/37/Everything/x86_64/repodata/repomd.xml
-            rpm.RpmMirror('http://mirrors.rit.edu/fedora/fedora/linux/updates/', 'Everything/{}/'.format(crawler_filter.machine), repo_filter),
+            rpm.RpmMirror('https://fedora.osuosl.org/linux/updates/', 'Everything/{}/'.format(crawler_filter.machine), repo_filter),
         ]
         return mirrors
